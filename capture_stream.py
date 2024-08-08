d70b0f567f3d5f7c7e9643cd780b8e735453e493
Badies4Life <tientonybest2006@gmail.com>
2024-08-09 09:57:05 +1000

    Prepare to include html template with some html and css customisations

--- capture_stream.py
+++ capture_stream.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from picamera2 import Picamera2
 import cv2
 from libcamera import Transform
@@ -46,9 +46,12 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
+@app.route('/')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/video_feed')
+def page_video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
